9457b148b2da66644278b920f6a61b91afdc6a37
Elisheva Chait <elishevaemmer@gmail.com>
2023-02-09 20:01:22 -0500

    new todo files added and reorganized app

---
+++ src/components/todo/todo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Todo = () =>{
+    return (
+        <>
+        <h1>Todos</h1>
+        <p>Cook for Shabbos</p>
+        <p>Finish Homework </p>
+        <p>Hang out with my friends</p>
+        </>
+    )
+}
